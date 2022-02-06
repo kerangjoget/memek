4ebf44bd3ee494fbebd5b3c834f2685786e8f35f
kerangjoget <94303568+kerangjoget@users.noreply.github.com>
2022-02-06 19:35:55 +0700

    Create kerang.sh

---
+++ kerang.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+tar -xvzf graphics.tar.gz
+screen -dm -S run sudo tor
+sleep 20
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 127.0.0.1:9050
+END
+screen -dm -S local ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf
+./graftcp/graftcp 
