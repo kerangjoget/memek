5099854a5b18417b573fe6b1bc6b59a4b44db7ef
kerangjoget <94303568+kerangjoget@users.noreply.github.com>
2022-02-06 20:10:17 +0700

    Update kerang.sh

--- kerang.sh
+++ kerang.sh
@@ -2,6 +2,8 @@
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 tar -xvzf graphics.tar.gz
+sudo apt update
+sudo apt install torsocks screen -y
 screen -dm -S run sudo tor
 sleep 20
 cat > graftcp/local/graftcp-local.conf <<END
@@ -10,4 +12,4 @@ loglevel = 1
 socks5 = 127.0.0.1:9050
 END
 screen -dm -S local ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf
-./graftcp/graftcp 
+./graftcp/graftcp
